aa26c6a62d18f52cc49618a5b0e492eec7d9e790
Glenn A Doherty <gadz@earthlink.net>
2019-08-23 18:59:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
-# Your Code Here
+def map (original_array)
+  new_array = []
+  original_array.length.times do |index|
+    new_array[index] = yield (original_array[index])
+  end
+  new_array
+end
+
+def reduce (original_array, starting_point = 0)
+  total = 0
+  original_array.length.times do |index|
+      # check for integer
+    if original_array[index].is_a? Integer
+      total=total+(original_array[index])
+    else 
+      all_false = false
+      all_true = true
+      original_array.length.times do |index|
+        if original_array[index] == false
+          all_true = false
+        else
+          all_false = true
+        end
+      end
+      # reduce to true/false
+      conclusion = yield(all_false, all_true)
+      return conclusion
+    end
+  end
+    # reduce to value
+    conclusion = yield(total, starting_point)
+    conclusion
+end
