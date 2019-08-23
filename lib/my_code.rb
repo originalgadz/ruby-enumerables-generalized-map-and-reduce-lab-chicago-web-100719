44eab310ee73e85e2163d12a8e3c0412e50b1724
Glenn A Doherty <gadz@earthlink.net>
2019-08-23 19:07:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,6 +10,7 @@ def reduce (original_array, starting_point = 0)
   total = 0
   original_array.length.times do |index|
       # check for integer
+<<<<<<< HEAD
     if original_array[index].is_a? Integer
       total=total+(original_array[index])
     else 
@@ -28,6 +29,26 @@ def reduce (original_array, starting_point = 0)
     end
   end
     # reduce to value
+=======
+      if original_array[index].is_a? Integer
+      total=total+(original_array[index])
+    else 
+      test1 = true
+      test2 = false
+      original_array.length.times do |index|
+        if original_array[index] == false
+          test1 = false
+        end
+      end
+      if test1 == true
+        test2 = true
+      end
+      conclusion = yield(test1, test2)
+      conclusion
+      break
+    end
+  end
+>>>>>>> 5c25f8e4a743dac5ff4c1b9f3a16e3e2838db2a0
     conclusion = yield(total, starting_point)
     conclusion
 end
